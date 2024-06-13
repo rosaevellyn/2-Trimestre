1836aeacce4dd7d52ca112343b81daaf5a11b2ee
rosaevellyn <165039637+rosaevellyn@users.noreply.github.com>
2024-06-13 11:54:02 -0300

    ;

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2049 Evellyn";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
